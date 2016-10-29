11f22602545f236dec4de7245b19286d0c95b7e6
Falco Bethke <fbethke@gmx.de>
2016-10-29 14:05:58 +0200

    Add project files.

---
+++ BranchingDemo/Account.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace BranchingDemo
+{
+    public class Account
+    {
+        public decimal Balance { get; private set; }
+
+        private IAccountState State { get; set; }
+
+        public Account(Action onUnfreeze)
+        {
+            this.State = new NotVerified(onUnfreeze);
+        }
+
+       
+        public void Deposit(decimal amount)
+        {
+            this.State = this.State.Deposit(() => { this.Balance += amount; });
+        }
+
+        // #3 (Interaction): Withdraw was invoked on the State
+        // #4 (Behavior): Result of State.Withdraw is new State
+        // #6 (Behavior): Deposit 1, Verify, Withdraw 1 - Balance == 9
+        public void Withdraw(decimal amount)
+        {
+            this.State = this.State.Withdraw(() => { this.Balance -= amount; });
+        }
+
+        public void HolderVerified()
+        {
+            this.State = this.State.HolderVerified();
+        }
+
+        public void Close()
+        {
+            this.State = this.State.Close();
+        }
+
+        public void Freeze()
+        {
+            this.State = this.State.Freeze();
+        }
+
+    }
+}
---
+++ BranchingDemoTests/UnitTest1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using BranchingDemo;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+
+namespace BranchingDemoTests {
+    [TestClass]
+    public class UnitTest1 {
+
+        Account a;
+        Action action = () => Debug.WriteLine("unfreeze");
+
+        [TestInitialize()]
+        public void Init() {
+            a = new Account(action);
+        }
+
+        [TestMethod]
+        public void Balance10() {
+            a.Deposit(10);
+            Assert.AreEqual(10, a.Balance);
+        }
+        [TestMethod]
+        public void withdraw() {
+            a.Withdraw(10);
+            Assert.AreEqual(0, a.Balance);
+        }
+        [TestMethod]
+        public void freezed() {
+
+            a.Deposit(10);
+            a.Freeze();
+            a.Withdraw(1);    
+            Assert.AreEqual(10, a.Balance);
+        }
+
+        [TestMethod]
+        public void freezed2() {
+
+            a.Deposit(10);
+           
+            a.Deposit(1); //wake up
+            Assert.AreEqual(11, a.Balance);
+            a.Withdraw(2);
+            Assert.AreEqual(11, a.Balance);
+            a.HolderVerified();
+            a.Withdraw(2);
+            Assert.AreEqual(9, a.Balance);
+            a.Freeze();
+            a.Withdraw(2);
+            Assert.AreEqual(7, a.Balance);
+
+        }
+
+
+    }
+}
